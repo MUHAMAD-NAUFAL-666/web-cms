89e18553d7286d099afb66639bb53033169bcabd
MuhamadNaufalAL <naufal.nadukar@gmail.com>
2025-07-19 15:55:46 +0700

    Menambahkan Crud Artikel

---
+++ app/Filament/Resources/LayananResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LayananResource\Pages;
+use App\Filament\Resources\LayananResource\RelationManagers;
+use App\Models\Layanan;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LayananResource extends Resource
+{
+    protected static ?string $model = Layanan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('nama')->required(),
+Textarea::make('deskripsi')->rows(3),
+FileUpload::make('icon')->image()->directory('icon-layanan'),
+TextInput::make('urutan')->numeric()->default(0),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLayanans::route('/'),
+            'create' => Pages\CreateLayanan::route('/create'),
+            'edit' => Pages\EditLayanan::route('/{record}/edit'),
+        ];
+    }
+}
